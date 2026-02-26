c2eab041dd632511320cbb53d1e9aa8b4bbd6d26
Your Name <georg3.gur@gmail.com>
2026-02-26 17:05:52 +0100

    chores

--- test_duration.rb
+++
@@ -1,6 +0,0 @@
-require './config/environment' if File.exist?('./config/environment.rb')
-require './app/controllers/application_controller'
-
-app = ApplicationController.new
-app.helpers.run_leaderboard_reset_job(force: true)
-puts "Leaderboard reset job ran!"
--- test_routes.rb
+++
@@ -1,8 +0,0 @@
-require './config/environment' if File.exist?('./config/environment.rb')
-require './app/controllers/application_controller'
-Dir["./app/controllers/*.rb"].each { |file| require file }
-
-puts "Routes in LeaderboardController:"
-LeaderboardController.routes['GET'].each do |route|
-  puts route[0].inspect
-end
